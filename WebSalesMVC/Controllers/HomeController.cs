961aaea0db6db5e22db2624f8d03581ebfc6ac83
Jonathan Loth <ninoloth@gmail.com>
2021-08-27 10:54:48 -0300

    First Model View Departmen

--- WebSalesMVC/Controllers/HomeController.cs
+++ WebSalesMVC/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using WebSalesMVC.Models;
+using WebSalesMVC.Models.ViewModels;
 
 namespace WebSalesMVC.Controllers
 {
