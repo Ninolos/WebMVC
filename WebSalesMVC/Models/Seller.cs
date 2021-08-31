81fb9acf3388aa4cd7d5b4d759ec4139d49c562e
Jonathan Loth <ninoloth@gmail.com>
2021-08-31 19:28:51 -0300

    Include validations

--- WebSalesMVC/Models/Seller.cs
+++ WebSalesMVC/Models/Seller.cs
@@ -9,15 +9,25 @@ namespace WebSalesMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="{0} required")] //validation
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1} characters")]
         public string Name { get; set; }
+
         [DataType(DataType.EmailAddress)] //customize the Display
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")] 
         [DataType(DataType.Date)]   
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
         [Display(Name = "Base Salary")] 
         [DisplayFormat(DataFormatString = "{0:F2}")] //format to two decimals
+        [Required(ErrorMessage = "{0} required")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; } //Entity Framework ensure that the DB contains an Id
         public int DepartmentId { get; set; }
